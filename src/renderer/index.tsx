123bac95ce20e9cb6aea790868f2de867fb7c021
André Jorge Lopes <andrelopes.professional@gmail.com>
2025-07-13 14:04:48 +0200

    switching to electron forge updating dependencies wip

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,10 +1,10 @@
-import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { Provider } from "react-redux";
 import App from "./App";
 import { PersistGate } from 'redux-persist/integration/react'
 import returnVar from './store/configureStore'
 
-const myVar = returnVar()
+const myVar = returnVar();
 
 
 declare global {
@@ -21,11 +21,15 @@ declare global {
   }
 }
 
-render(
 
-  <Provider store={myVar.reduxStore}>
+const container = document.getElementById('root');
+if (container != null) {
+const root = createRoot(container);
+root.render(
+   <Provider store={myVar.reduxStore}>
     <PersistGate loading={null} persistor={myVar.persistor}>
      <App />
      </PersistGate>
   </Provider>
-, document.getElementById("root"));
+);
+}
