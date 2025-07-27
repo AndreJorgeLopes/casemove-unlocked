611acac4cfdf9d940d5e306067c5bb6481204a94
Andre Jorge Lopes <andrelopes.professional@gmail.com>
2025-07-27 20:33:18 +0200

    fix: data load images from different cdn

--- src/renderer/functionsClasses/createCSGOImage.tsx
+++ src/renderer/functionsClasses/createCSGOImage.tsx
@@ -1,3 +1,7 @@
 export function createCSGOImage ( urlEndpath : string ) : string {
+<<<<<<< HEAD
   return 'https://images.skinledger.com/counterstrike/' + urlEndpath + '.png'
+=======
+  return 'https://raw.githubusercontent.com/SkinDelta/go-cs2-cdn/refs/heads/main/static/panorama/images/' + urlEndpath + '.png'
+>>>>>>> fix(data): load images from different cdn and also items
 }
