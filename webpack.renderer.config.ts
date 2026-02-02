903c175126edd71dc5a733d429119212553c23ea
Andre Jorge Lopes <andrelopes.professional@gmail.com>
2026-02-02 21:14:23 +0000

    Set Electron webpack targets

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -4,11 +4,15 @@ import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
 export const rendererConfig: Configuration = {
+  target: 'electron-renderer',
   module: {
     rules,
   },
   plugins,
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    fallback: {
+      memcpy: false,
+    },
   },
 };
