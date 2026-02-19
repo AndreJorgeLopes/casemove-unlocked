14a7748222da1f36112d48c51bf8ccc6242a5946
André Jorge Lopes <andrelopes.professional@gmail.com>
2026-02-19 06:45:40 +0000

    Unblock cleanup by fixing renderer companion import lint failure

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,16 +1,15 @@
 import { createRoot } from 'react-dom/client';
-import { Provider } from "react-redux";
-import App from "./App";
+import { Provider } from 'react-redux';
+import App from './App';
+// eslint-disable-next-line import/no-unresolved
 import { VibeKanbanWebCompanion } from 'vibe-kanban-web-companion';
-import { PersistGate } from 'redux-persist/integration/react'
-import returnVar from './store/configureStore'
+import { PersistGate } from 'redux-persist/integration/react';
+import returnVar from './store/configureStore';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import React from 'react';
 
-
 const myVar = returnVar();
-
 
 declare global {
   interface Window {
@@ -20,27 +19,25 @@ declare global {
         get: (key: string) => any;
         set: (key: string, val: any) => void;
         // any other methods you've defined...
-      },
-      ipcRenderer: any
-    }
-
+      };
+      ipcRenderer: any;
+    };
   }
 }
 
-
 const container = document.getElementById('root');
 if (container != null) {
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-  <Router>
-      <Provider store={myVar.reduxStore}>
-        <PersistGate loading={null} persistor={myVar.persistor}>
-        <App />
-        <VibeKanbanWebCompanion />
-        </PersistGate>
-      </Provider>
-  </Router>
-   </React.StrictMode>
-);
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <Provider store={myVar.reduxStore}>
+          <PersistGate loading={null} persistor={myVar.persistor}>
+            <App />
+            <VibeKanbanWebCompanion />
+          </PersistGate>
+        </Provider>
+      </Router>
+    </React.StrictMode>,
+  );
 }
